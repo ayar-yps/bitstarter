c5a1bafbca7d68d5f5a9ce0fdea2f355bcac9319
ayar-yps <ayar.yps@gmail.com>
2013-07-11 06:46:12 +0000

    web.js fixed

--- web.js
+++ web.js
@@ -4,9 +4,9 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs=require('fs');
-  
+
   //var content=fs.readFileSync('index.html',"utf-8");
-  var buffer = new Buffer(fs.redFileSync('index.html'),'utf-8');
+  var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
   //Responde.send(content);
   Response.send(buffer.toString());
 
