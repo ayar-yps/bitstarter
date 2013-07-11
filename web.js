7dc5ba3555de4166d5a0fd2f67b991b48c7d4505
ayar-yps <ayar.yps@gmail.com>
2013-07-11 06:42:40 +0000

    other version of web.js

--- web.js
+++ web.js
@@ -4,8 +4,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs=require('fs');
-  var content=fs.readFileSync('index.html',"utf-8");
-  response.send(content);
+  
+  //var content=fs.readFileSync('index.html',"utf-8");
+  var buffer = new Buffer(fs.redFileSync('index.html'),'utf-8');
+  //Responde.send(content);
+  Response.send(buffer.toString());
+
 });
 
 var port = process.env.PORT || 5000;
