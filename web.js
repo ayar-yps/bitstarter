8a41409749b6ed418ef57b374c777b1ed9dda123
ayar-yps <ayar.yps@gmail.com>
2013-07-11 06:33:17 +0000

    new web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer=new Buffer(fs.readFileSync(index.html,"r"),"utf-8")
-
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'));
+  var fs=require('fs');
+  var content=fs.readFileSync('index.html',"utf-8");
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
